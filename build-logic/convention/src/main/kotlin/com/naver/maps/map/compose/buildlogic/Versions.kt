6d035b09a79065e22efa30ef60d0e8267c1fba32
Sungyong An <sungyong.an@webtoonscorp.com>
2024-11-23 23:14:22 +0900

    Fix wrong compileSdk

--- build-logic/convention/src/main/kotlin/com/naver/maps/map/compose/buildlogic/Versions.kt
+++ build-logic/convention/src/main/kotlin/com/naver/maps/map/compose/buildlogic/Versions.kt
@@ -2,6 +2,6 @@ package com.naver.maps.map.compose.buildlogic
 
 object Versions {
     const val minSdk = 21
-    const val compileSdk = 33
-    const val targetSdk = 35
+    const val compileSdk = 35
+    const val targetSdk = 33
 }
